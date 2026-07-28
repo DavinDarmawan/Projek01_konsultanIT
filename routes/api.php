746f452356c34a7671faf0d2ae5739c085069f4e
Hafizh Almaarif Karim Amrullah <hafizhalmaarifka@gmail.com>
2026-07-28 13:47:58 +0700

    feat: membuat endpoint API untuk Service dan Portfolio

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ServiceController;
+use App\Http\Controllers\Api\PortfolioController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/services', [ServiceController::class, 'indexPublic']);
+Route::get('/services/{slug}', [ServiceController::class, 'showPublic']);
+
+Route::get('/portfolios', [PortfolioController::class, 'indexPublic']);
+Route::get('/portfolios/{id}', [PortfolioController::class, 'showPublic']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('admin/services', ServiceController::class);
+    Route::apiResource('admin/portfolios', PortfolioController::class);
 });
