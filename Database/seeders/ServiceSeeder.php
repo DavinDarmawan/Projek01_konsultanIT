0791ab67ec73fd3f2ef989d5cf05a89d9395d185
Ahmad Kurnia <ahmadkurnia779@gmail.com>
2026-07-28 14:06:08 +0700

    feat(database): complete migration, models, seeders and relationships

--- Database/seeders/ServiceSeeder.php
+++ Database/seeders/ServiceSeeder.php
@@ -2,16 +2,60 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Service;
 
 class ServiceSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $services = [
+
+            [
+                'title' => 'Software Development',
+                'slug' => 'software-development'
+            ],
+
+            [
+                'title' => 'Website CMS',
+                'slug' => 'website-cms'
+            ],
+
+            [
+                'title' => 'E-Raport Sekolah',
+                'slug' => 'e-raport'
+            ],
+
+            [
+                'title' => 'Kehosting.in',
+                'slug' => 'kehosting'
+            ],
+
+            [
+                'title' => 'Legal Dari Kita',
+                'slug' => 'legal-dari-kita'
+            ],
+
+            [
+                'title' => 'Training',
+                'slug' => 'training'
+            ],
+
+            [
+                'title' => 'Balanja.id',
+                'slug' => 'balanja-id'
+            ]
+        ];
+
+        foreach ($services as $service) {
+
+            Service::create([
+                'title' => $service['title'],
+                'slug' => $service['slug'],
+                'description' => $service['title'].' Service',
+                'status' => 'active',
+                'created_by' => 1
+            ]);
+        }
     }
 }
