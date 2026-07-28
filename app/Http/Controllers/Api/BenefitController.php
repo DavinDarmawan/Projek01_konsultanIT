2bd773e88448d99ce866c5e40c0422b855cdf12c
Ahmad Kurnia <ahmadkurnia779@gmail.com>
2026-07-28 15:07:15 +0700

    feat(database): add benefit module and service relationship

---
+++ app/Http/Controllers/Api/BenefitController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Benefit;
+use Illuminate\Http\Request;
+
+class BenefitController extends Controller
+{
+    public function indexPublic()
+    {
+        return Benefit::with('service')->get();
+    }
+
+    public function showPublic($id)
+    {
+        return Benefit::with('service')
+            ->findOrFail($id);
+    }
+
+    public function index()
+    {
+        return Benefit::all();
+    }
+
+    public function store(Request $request)
+    {
+        return Benefit::create(
+            $request->all()
+        );
+    }
+
+    public function show(Benefit $benefit)
+    {
+        return $benefit;
+    }
+
+    public function update(Request $request, Benefit $benefit)
+    {
+        $benefit->update(
+            $request->all()
+        );
+
+        return $benefit;
+    }
+
+    public function destroy(Benefit $benefit)
+    {
+        $benefit->delete();
+
+        return response()->json([
+            'message' => 'Deleted'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\Api\PortfolioController;
-
-
+use App\Http\Controllers\Api\BenefitController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+// PUBLIC API
 
 Route::get('/services', [ServiceController::class, 'indexPublic']);
 Route::get('/services/{slug}', [ServiceController::class, 'showPublic']);
@@ -18,7 +20,27 @@ Route::get('/services/{slug}', [ServiceController::class, 'showPublic']);
 Route::get('/portfolios', [PortfolioController::class, 'indexPublic']);
 Route::get('/portfolios/{id}', [PortfolioController::class, 'showPublic']);
 
+Route::get('/benefits', [BenefitController::class, 'indexPublic']);
+Route::get('/benefits/{id}', [BenefitController::class, 'showPublic']);
+
+
+// ADMIN API
+
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('admin/services', ServiceController::class);
-    Route::apiResource('admin/portfolios', PortfolioController::class);
+
+    Route::apiResource(
+        'admin/services',
+        ServiceController::class
+    );
+
+    Route::apiResource(
+        'admin/portfolios',
+        PortfolioController::class
+    );
+
+    Route::apiResource(
+        'admin/benefits',
+        BenefitController::class
+    );
+
 });
