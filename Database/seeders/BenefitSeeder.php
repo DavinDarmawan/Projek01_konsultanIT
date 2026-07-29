23e3fce829ad8759fa0379c87ee1e716c1f44fd5
Ahmad Kurnia <ahmadkurnia779@gmail.com>
2026-07-29 11:27:34 +0700

    feat(database): complete seeders, dummy data, team and partner modules

--- Database/seeders/BenefitSeeder.php
+++ Database/seeders/BenefitSeeder.php
@@ -2,19 +2,39 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Benefit;
 
-class DatabaseSeeder extends Seeder
+class BenefitSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call([
-            UserSeeder::class,
-            HeroSectionSeeder::class,
-            ServiceSeeder::class,
-            PortfolioSeeder::class,
-            BenefitSeeder::class,
+        Benefit::insert([
+
+            [
+                'service_id' => 1,
+                'title' => 'Custom Business Solution',
+                'description' => 'Aplikasi sesuai kebutuhan bisnis'
+            ],
+
+            [
+                'service_id' => 1,
+                'title' => 'Scalable Architecture',
+                'description' => 'Mudah dikembangkan dan scalable'
+            ],
+
+            [
+                'service_id' => 2,
+                'title' => 'SEO Friendly',
+                'description' => 'Mudah ditemukan di mesin pencari'
+            ],
+
+            [
+                'service_id' => 2,
+                'title' => 'Responsive Design',
+                'description' => 'Tampilan optimal di semua perangkat'
+            ]
+
         ]);
     }
 }
